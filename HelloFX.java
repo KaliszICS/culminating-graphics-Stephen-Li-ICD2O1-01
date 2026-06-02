a7d941c2a66c04575662ef5bcce0a1e3529daff9
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2026-06-02 15:41:33 +0000

    day2

--- HelloFX.java
+++ HelloFX.java
@@ -15,7 +15,11 @@ import javafx.scene.layout.VBox; //added
 import javafx.stage.Stage;
 import javafx.scene.shape.Circle; //added
 import javafx.scene.shape.Rectangle;
-import javafx.geometry.Pos; //added (tells VBox positions)
+import javafx.geometry.Pos; //added (tells positions on elements)
+import javafx.geometry.Insets; //added
+import javafx.scene.text.Font; //added
+import javafx.scene.paint.Color; //added
+import javafx.scene.input.KeyCode; //added
 
 public class HelloFX extends Application {
 
@@ -23,9 +27,15 @@ public class HelloFX extends Application {
     public void start(Stage stage) {
         //Menu Setup
         Label l = new Label("Brick Breaker Game");
+        l.setFont(new Font("", 30));
+        l.setTextFill(Color.RED); //make background black and text white
         Button startButton = new Button("Start Game");
-        Pane menuLayer = new Pane();
-        menuLayer.getChildren().add(l);
+        StackPane menuLayer = new StackPane();
+        StackPane.setAlignment(l, Pos.TOP_CENTER); //Positions title
+        StackPane.setMargin(l, new Insets(50, 8, 8, 8));
+        StackPane.setAlignment(startButton, Pos.BOTTOM_CENTER); //Positions button
+        StackPane.setMargin(startButton, new Insets(8, 8, 50, 8));
+        menuLayer.getChildren().add(l); //Adds all elements into the menu
         menuLayer.getChildren().add(startButton);
         Scene menuScene = new Scene(menuLayer, 640, 480);
 
@@ -42,6 +52,23 @@ public class HelloFX extends Application {
 
         Scene gameScene = new Scene(gameLayer, 640, 480);
 
+        //Paddle movement
+        gameScene.setOnKeyPressed(event -> {
+            double paddleX = paddle.getX();
+            int paddleSpeed = 15;
+            if (event.getCode() == KeyCode.A) {
+                if (paddleX > 0) { //Prevent going through left wall
+                    paddle.setX(paddleX - paddleSpeed);
+                }
+            }
+            else if (event.getCode() == KeyCode.D) { //repeart for arrow keys
+                if (paddleX < 640 - paddle.getWidth()) { //Prevent going past right wall
+                    paddle.setX(paddleX + paddleSpeed);
+                }
+            }
+        });
+
+        //Button action on click
         startButton.setOnAction(e -> {
             stage.setScene(gameScene);
         });
