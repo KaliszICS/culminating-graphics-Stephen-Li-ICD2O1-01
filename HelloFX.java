b70b7aca6aeb66379eea4e24e434ac1c47e3b20e
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2026-06-03 15:42:05 +0000

    day3

--- HelloFX.java
+++ HelloFX.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Button; //added
 import javafx.scene.layout.Pane; //added
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox; //added
 import javafx.stage.Stage;
 import javafx.scene.shape.Circle; //added
 import javafx.scene.shape.Rectangle;
@@ -20,21 +19,30 @@ import javafx.geometry.Insets; //added
 import javafx.scene.text.Font; //added
 import javafx.scene.paint.Color; //added
 import javafx.scene.input.KeyCode; //added
+import javafx.animation.AnimationTimer; //added
+import javafx.scene.layout.Background; //added
+import javafx.scene.layout.BackgroundFill; //added
 
 public class HelloFX extends Application {
+    //Track if keys for paddle are pressed
+    boolean leftPressed = false;
+    boolean rightPressed = false;
 
     @Override
     public void start(Stage stage) {
         //Menu Setup
         Label l = new Label("Brick Breaker Game");
-        l.setFont(new Font("", 30));
-        l.setTextFill(Color.RED); //make background black and text white
+        l.setFont(new Font("Arial", 30));
+        l.setTextFill(Color.WHITE); //make background black and text white
         Button startButton = new Button("Start Game");
+
         StackPane menuLayer = new StackPane();
+        menuLayer.setBackground(new Background(new BackgroundFill(Color.DARKGRAY, null, null)));
         StackPane.setAlignment(l, Pos.TOP_CENTER); //Positions title
         StackPane.setMargin(l, new Insets(50, 8, 8, 8));
         StackPane.setAlignment(startButton, Pos.BOTTOM_CENTER); //Positions button
         StackPane.setMargin(startButton, new Insets(8, 8, 50, 8));
+
         menuLayer.getChildren().add(l); //Adds all elements into the menu
         menuLayer.getChildren().add(startButton);
         Scene menuScene = new Scene(menuLayer, 640, 480);
@@ -52,25 +60,80 @@ public class HelloFX extends Application {
 
         Scene gameScene = new Scene(gameLayer, 640, 480);
 
-        //Paddle movement
+        //Checking if key pressed using booleans to have smooth paddle movement
         gameScene.setOnKeyPressed(event -> {
-            double paddleX = paddle.getX();
-            int paddleSpeed = 15;
-            if (event.getCode() == KeyCode.A) {
-                if (paddleX > 0) { //Prevent going through left wall
-                    paddle.setX(paddleX - paddleSpeed);
-                }
+            if (event.getCode() == KeyCode.A || event.getCode() == KeyCode.LEFT) {
+                leftPressed = true;
             }
-            else if (event.getCode() == KeyCode.D) { //repeart for arrow keys
-                if (paddleX < 640 - paddle.getWidth()) { //Prevent going past right wall
-                    paddle.setX(paddleX + paddleSpeed);
-                }
+            if (event.getCode() == KeyCode.D || event.getCode() == KeyCode.RIGHT) {
+                rightPressed = true;
+            }
+        });
+        
+        gameScene.setOnKeyReleased(event -> {
+            if (event.getCode() == KeyCode.A || event.getCode() == KeyCode.LEFT) {
+                leftPressed = false;
+            }
+            if (event.getCode() == KeyCode.D || event.getCode() == KeyCode.RIGHT) {
+                rightPressed = false;
             }
         });
 
+        //Paddle movement
+        AnimationTimer gameLoop = new AnimationTimer() {
+            public void handle(long time) { //Checks time in nanosecond
+                double paddleX = paddle.getX();
+                int paddleSpeed = 6;
+                if (leftPressed == true && paddleX > 0) {
+                    paddle.setX(paddleX - paddleSpeed);
+                }
+                else if (rightPressed == true && (paddleX < 640 - paddle.getWidth())) { //repeat for arrow keys
+                    paddle.setX(paddleX + paddleSpeed);
+                }
+            }
+        };
+        
+        //Countdown for ball to move
+        Label countdownTimer = new Label("3");
+        countdownTimer.setFont(new Font("Arial", 60));
+        countdownTimer.setTextFill(Color.WHITE);
+        countdownTimer.setLayoutX(300);
+        countdownTimer.setLayoutY(200);
+        long[] startTime = {0};
+        AnimationTimer ballStart = new AnimationTimer() {
+            public void handle(long time) {
+                if (startTime[0] == 0) {
+                    startTime[0] = time;
+                    gameLayer.getChildren().add(countdownTimer);
+                }
+                long elaspedTime = time;
+                if (elaspedTime == 1L) {
+                    l = 3;
+                    gameLayer.getChildren().add(l);
+                }
+                if (elaspedTime == 1_000_000_000L) {
+                    gameLayer.getChildren().remove(l);
+                    l = 2;
+                    gameLayer.getChildren().add(l);
+                }
+                if (elaspedTime == 2_000_000_000L) {
+                    gameLayer.getChildren().remove(l);
+                    l = 1;
+                    gameLayer.getChildren().add(l);
+                }
+                if (elaspedTime == 3_000_000_000L) {
+                    gameLayer.getChildren().remove(l);
+                    l = "go";
+                    gameLayer.getChildren().add(l);
+                }
+            }
+        };
+
         //Button action on click
-        startButton.setOnAction(e -> {
+        startButton.setOnAction(event -> {
             stage.setScene(gameScene);
+            gameLoop.start();
+            ballStart.start();
         });
 
         stage.setTitle("Brick Breaker Game");
